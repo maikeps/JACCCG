5f9c1bb083e21b61e723fd45f1b2b4d2cd447a90
Maikeps <psmaike@gmail.com>
2014-10-15 09:27:32 +0000

    RegistroDeBaralho e Baralho codificados

--- Batalha/Baralho.java
+++ Batalha/Baralho.java
@@ -1,20 +1,29 @@
 package JACCCG.Batalha;
 
-import JACCCG.Colecao.RegistroDeBaralho;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
 import JACCCG.Cartas.Carta;
 import JACCCG.Cartas.CartaDeBatalha;
-import java.util.Stack;
+import JACCCG.Colecao.RegistroDeBaralho;
 
 public class Baralho {
 
 	private Stack<CartaDeBatalha> pilhaDeCartas;
 
 	public Baralho(RegistroDeBaralho baralho) {
-
+		List<Carta> copia = baralho.getCartas();
+		pilhaDeCartas = new Stack<CartaDeBatalha>();
+		for(Carta carta : copia){
+			CartaDeBatalha cb = new CartaDeBatalha(carta.getNome(), carta.getAtaque(), carta.getDefesa(), carta.getCustoMana(), carta.getVida(), carta.getRaridade());
+			pilhaDeCartas.add(cb);
+		}
+		Collections.shuffle(pilhaDeCartas);
 	}
 
 	public Carta getTopo() {
-		return null;
+		return pilhaDeCartas.pop();
 	}
 
 }
