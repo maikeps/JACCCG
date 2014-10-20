9c6856cb8ee8605d25ef3d08e5be30b4b32b21e4
Maikeps <psmaike@gmail.com>
2014-10-20 14:59:01 +0000

    a

--- JACCCG/Cartas/FabricaDeCartas.java
+++ JACCCG/Cartas/FabricaDeCartas.java
@@ -5,15 +5,15 @@ import java.util.List;
 
 public class FabricaDeCartas {
 
-	public static List<Carta> criaCartas() {
-		List<Carta> listaDeCartas = new LinkedList<Carta>();
+	public static List<CartaDeColecao> criaCartas() {
+		List<CartaDeColecao> listaDeCartas = new LinkedList<CartaDeColecao>();
 		
-		listaDeCartas.add(new Carta("assovio",2,3,8,2048,Raridade.COMUM));
-		listaDeCartas.add(new Carta("assovio2",22,32,82,22048,Raridade.COMUM));
-		listaDeCartas.add(new Carta("assovi3o",2,3,8,20482,Raridade.COMUM));
-		listaDeCartas.add(new Carta("assov3io",2,3,8,2048,Raridade.COMUM));
-		listaDeCartas.add(new Carta("assovi4o",2,3,8,2048,Raridade.COMUM));
-		listaDeCartas.add(new Carta("assovi5o",2,3,8,2048,Raridade.COMUM));
+		listaDeCartas.add(new CartaDeColecao("carta1",2,3,8,2048,Raridade.COMUM, 1));
+		listaDeCartas.add(new CartaDeColecao("carta2",22,32,82,22048,Raridade.COMUM, 2));
+		listaDeCartas.add(new CartaDeColecao("carta3",2,3,8,20482,Raridade.COMUM, 3));
+		listaDeCartas.add(new CartaDeColecao("carta4",2,3,8,2048,Raridade.COMUM, 4));
+		listaDeCartas.add(new CartaDeColecao("carta5",2,3,8,2048,Raridade.COMUM, 5));
+		listaDeCartas.add(new CartaDeColecao("carta6",2,3,8,2048,Raridade.COMUM, 600));
 		
 		return  listaDeCartas;
 	}
