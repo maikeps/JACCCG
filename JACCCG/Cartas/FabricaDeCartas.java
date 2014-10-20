441fd4d9d31ca3e6cfb3d29da089c60131522bc1
matheusteixeira <matheusteixeira01@live.com>
2014-10-20 14:55:18 -0200

    Loja e "fabrica" salve testes

--- JACCCG/Cartas/FabricaDeCartas.java
+++ JACCCG/Cartas/FabricaDeCartas.java
@@ -8,13 +8,21 @@ public class FabricaDeCartas {
 	public static List<Carta> criaCartas() {
 		List<Carta> listaDeCartas = new LinkedList<Carta>();
 		
-		listaDeCartas.add(new Carta("assovio",2,3,8,2048,Raridade.COMUM));
-		listaDeCartas.add(new Carta("assovio2",22,32,82,22048,Raridade.COMUM));
-		listaDeCartas.add(new Carta("assovi3o",2,3,8,20482,Raridade.COMUM));
-		listaDeCartas.add(new Carta("assov3io",2,3,8,2048,Raridade.COMUM));
-		listaDeCartas.add(new Carta("assovi4o",2,3,8,2048,Raridade.COMUM));
-		listaDeCartas.add(new Carta("assovi5o",2,3,8,2048,Raridade.COMUM));
-		
+		listaDeCartas.add(new CartaDeColecao("Goblin1", 1, 1, 1, 1, Raridade.COMUM, 100));
+		listaDeCartas.add(new CartaDeColecao("Goblin2", 1, 2, 1, 1, Raridade.COMUM, 110));
+		listaDeCartas.add(new CartaDeColecao("Goblin3", 2, 1, 1, 1, Raridade.INCOMUM, 201));
+		listaDeCartas.add(new CartaDeColecao("Goblin4", 2, 2, 2, 2, Raridade.INCOMUM, 205));
+		listaDeCartas.add(new CartaDeColecao("Goblin5", 3, 2, 2, 3, Raridade.RARA, 650));
+		listaDeCartas.add(new CartaDeColecao("Humanoide1", 1, 2, 1, 2, Raridade.COMUM, 50));
+		listaDeCartas.add(new CartaDeColecao("Humanoide2", 1, 2, 1, 4, Raridade.COMUM, 130));
+		listaDeCartas.add(new CartaDeColecao("Humanoide3", 2, 2, 2, 2, Raridade.INCOMUM, 290));
+		listaDeCartas.add(new CartaDeColecao("Humanoide4", 2, 3, 2, 3, Raridade.INCOMUM, 140));
+		listaDeCartas.add(new CartaDeColecao("Humanoide5", 2, 3, 3, 3, Raridade.RARA, 540));
+		listaDeCartas.add(new CartaDeColecao("Fera1", 4, 0, 4, 2, Raridade.INCOMUM, 300));
+		listaDeCartas.add(new CartaDeColecao("Fera2", 5, 0, 2, 2, Raridade.RARA, 500));
+		listaDeCartas.add(new CartaDeColecao("Monstruoso1", 6, 3, 6, 4, Raridade.INCOMUM, 350));
+		listaDeCartas.add(new CartaDeColecao("Monstruoso2", 5, 2, 5, 4, Raridade.RARA, 600));
+
 		return  listaDeCartas;
 	}
 }
